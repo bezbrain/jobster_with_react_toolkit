ccfc13e852d89d0608849d14910eb99e3da0673b
bezbrain <belloquadriolawale@gmail.com>
2023-07-27 05:07:48 -0700

    Setting up the register form input

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import errorImg from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={errorImg} alt="not found" />
+        <h3>Oh! Page Not Found</h3>
+        <p>We can't seem to find the page you're looking for.</p>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setVaslues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* Name field */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            className="form-input"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
